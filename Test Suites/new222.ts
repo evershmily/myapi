eabc3f0c53d103df0bd5e54d3a5ee2ea406d2aa8
sunjin0420 <sunjin0420@126.com>
2019-08-26 16:56:09 +0800

    branch

---
+++ Test Suites/new222.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>new222</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>45</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>eb7c8bc3-4a60-4495-af0a-aa46c40d4968</testSuiteGuid>
+   <testCaseLink>
+      <guid>e52a8f90-6d2e-4a5d-a04b-550a7a095b09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mykpi/newbran22</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
