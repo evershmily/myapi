70d05ae95412d88c0856a0d8dc98597efef86fa0
sunjin0420 <sunjin0420@126.com>
2019-08-26 17:15:19 +0800

    another branch

---
+++ Test Suites/another.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>another</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>45</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8e6db470-c702-4734-bca7-812572b825fa</testSuiteGuid>
+   <testCaseLink>
+      <guid>c573d784-0b8e-48d8-92b0-40b093601162</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mykpi/another</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
