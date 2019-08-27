86fd6cdab9166fca9578b090f060c31513e29ebb
sunjin0420 <sunjin0420@126.com>
2019-08-27 10:09:08 +0800

    new0827

---
+++ Test Suites/new0827.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>new0827</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>45</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3258669b-26bb-4011-93ab-b169d57d2c14</testSuiteGuid>
+   <testCaseLink>
+      <guid>7512267e-6d1f-4e6b-a19d-361a02148942</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mykpi/0827newbran</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
